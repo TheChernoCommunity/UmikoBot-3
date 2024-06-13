f8698adc1d097a6c0f06e72c1f38730743cdc418
FancyKillerPanda <fancykillerpanda@gmail.com>
2024-06-13 13:09:59 -0400

    Changed wallet reply to an embed.

--- src/commands/currency.cpp
+++ src/commands/currency.cpp
@@ -14,10 +14,20 @@ static void handle_daily(UmikoBot& bot, const dpp::slashcommand_t& event) {
 }
 
 static void handle_wallet(UmikoBot& bot, const dpp::slashcommand_t& event) {
-    GuildData& guildData = bot.get_guild_data(event);
-    UserData& userData   = bot.get_user_data(guildData);
+    const dpp::user& issuingUser = event.command.get_issuing_user();
 
-    event.reply(format_to_string("You have **%g** %s.", userData.coinsInCents / 100.0f, guildData.currencyLongName.c_str()));
+    GuildData& guildData = bot.get_guild_data(event);
+    UserData& userData   = bot.get_user_data(event);
+
+    dpp::embed embed {};
+    embed.set_author(format_to_string("%s's Wallet", issuingUser.global_name.c_str()), "", issuingUser.get_avatar_url());
+
+    embed.add_field("Current Credits",
+                    format_to_string("%g %s", userData.coinsInCents / 100.0f, guildData.currencyLongName.c_str()));
+    embed.add_field("Daily Streak", "0 (next bonus at 3)"); // @Incomplete.
+    embed.add_field("Today's daily claimed?", "No"); // @Incomplete.
+
+    event.reply(dpp::message { event.command.channel_id, embed });
 }
 
 void register_currency_commands(UmikoBot& bot) {
