7ddcd5790361345a8ec206c5d2d6a2d7dc455df2
FancyKillerPanda <fancykillerpanda@gmail.com>
2024-06-13 11:42:12 -0400

    Changed %f specifiers for currency to %g.

--- src/commands/currency.cpp
+++ src/commands/currency.cpp
@@ -9,14 +9,14 @@ static void handle_daily(UmikoBot& bot, const dpp::slashcommand_t& event) {
     userData.coinsInCents       += AMOUNT_TO_ADD;
 
     // @Settings.
-    event.reply(format_to_string("You now have **%f** more coins in your wallet!", userData.coinsInCents / 100.0f));
+    event.reply(format_to_string("You now have **%g** more coins in your wallet!", userData.coinsInCents / 100.0f));
 }
 
 static void handle_wallet(UmikoBot& bot, const dpp::slashcommand_t& event) {
     UserData& userData = bot.get_user_data(event);
 
     // @Settings.
-    event.reply(format_to_string("You have **%f** coins.", userData.coinsInCents / 100.0f));
+    event.reply(format_to_string("You have **%g** coins.", userData.coinsInCents / 100.0f));
 }
 
 void register_currency_commands(UmikoBot& bot) {
